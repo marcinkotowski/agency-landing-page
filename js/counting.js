f90a46eb10d8fe9c2402a56ace0778b326072786
marcinkotowski <marcin.kotowski02@gmail.com>
2023-04-08 17:19:07 +0200

    Add Animate On Scroll

---
+++ js/counting.js
@@ -0,0 +1,23 @@
+const countCoustomers = document.querySelector("#count-customers");
+
+const observer = new IntersectionObserver((entry) => {
+  if (entry[0].isIntersecting) {
+    counting();
+    observer.unobserve(countCoustomers);
+  }
+});
+
+observer.observe(countCoustomers);
+
+function counting() {
+  let expected = 100;
+  let current = 1;
+  countCoustomers.innerHTML = `${current}+`;
+  const counterInterval = setInterval(() => {
+    countCoustomers.innerHTML = `${current}+`;
+    if (current == expected) {
+      clearInterval(counterInterval);
+    }
+    current++;
+  }, 25);
+}
